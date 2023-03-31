b0267c4c5dd7924fa545bb27675e4085838f7601
rashidbik <abdurashid.bigzad1999@gmail.com>
2023-03-31 18:26:16 +0430

    sorting changed

--- src/routes/pages/reports/+server.js
+++ src/routes/pages/reports/+server.js
@@ -2,15 +2,30 @@ import { json } from '@sveltejs/kit';
 import { data } from '$lib/data/db';
 
 export const GET = () => {
-	let expens = 0;
-	let income = 0;
+	/**
+	 * @type {number[]}
+	 */
+	let expens = [];
+	/**
+	 * @type {number[]}
+	 */
+	let income = [];
 	data[0].contents.map((content) => {
 		if (content.type == 'expens') {
-			expens += content.amount;
+			expens.push(content.amount);
 		}
 		if (content.type == 'income') {
-			income += content.amount;
+			income.push(content.amount);
 		}
 	});
-	return json({ contents: data[0].contents, expens, income });
+	let count1 = 0;
+	for (let i = 0; i < expens.length; i++) {
+		count1 += expens[i];
+	}
+	let count2 = 0;
+	for (let i = 0; i < income.length; i++) {
+		count2 += income[i];
+	}
+
+	return json({ contents: data[0].contents, expens: count1, income: count2 });
 };
